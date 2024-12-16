135026121257fb2234fda5ae573901a75a85cb31
Andrew Nowak <andrew@nowak.me.uk>
2024-12-16 10:27:04 +0000

    2024: day 16 p1 (p2 working included but commented)

---
+++ 2024/src/bin/day16.rs
@@ -0,0 +1,143 @@
+use std::cmp::Reverse;
+use std::time::Instant;
+
+use aoclib::Direction;
+use priority_queue::PriorityQueue;
+use rustc_hash::FxHashSet as HashSet;
+
+use aoclib::Point;
+
+#[derive(Clone, Debug, PartialEq, Eq, Hash)]
+struct State {
+    score: i64,
+    //visited: Vec<Point>,
+    location: Point,
+    direction: Direction,
+}
+
+fn run(data: &str) {
+    let start = Instant::now();
+
+    let data = data.as_bytes();
+    let area_width = data.iter().position(|&b| b == b'\n').expect("no newlines?") + 1;
+
+    let startpoint = data.iter().position(|&b| b == b'S').expect("no start?");
+    let startpoint = Point {
+        x: (startpoint % area_width) as i32,
+        y: (startpoint / area_width) as i32,
+    };
+    let endpoint = data.iter().position(|&b| b == b'E').expect("no end?");
+    let endpoint = Point {
+        x: (endpoint % area_width) as i32,
+        y: (endpoint / area_width) as i32,
+    };
+    let walls = data
+        .iter()
+        .enumerate()
+        .filter_map(|(i, &b)| {
+            if b == b'#' {
+                Some(Point {
+                    x: (i % area_width) as i32,
+                    y: (i / area_width) as i32,
+                })
+            } else {
+                None
+            }
+        })
+        .collect::<HashSet<Point>>();
+
+    let mut startpoint_vec = Vec::with_capacity(50);
+    startpoint_vec.push(startpoint);
+
+    let initial = State {
+        score: 0,
+        //visited: startpoint_vec,
+        location: startpoint,
+        direction: Direction::Right,
+    };
+
+    let mut pq = PriorityQueue::with_capacity(100_000);
+    //let mut visited: HashMap<(Point, Direction), i64> =
+    //    HashMap::from_iter([((startpoint, Direction::Right), 0)]);
+    pq.push(initial.clone(), Reverse(0));
+
+    let mut best = i64::MAX;
+    //let mut best_paths = HashSet::default();
+
+    'mainloop: while let Some((state, _)) = pq.pop() {
+        if state.score > best {
+            break;
+        }
+        let mut turn_cw = state.clone();
+        turn_cw.direction = turn_cw.direction.cw();
+        turn_cw.score += 1000;
+        let mut turn_ccw = state.clone();
+        turn_ccw.direction = turn_ccw.direction.ccw();
+        turn_ccw.score += 1000;
+        let ahead = state.location.go(&state.direction);
+        let forward = if walls.contains(&ahead) {
+            None
+        } else {
+            let mut s = state.clone();
+            s.location = ahead;
+            //s.visited.push(ahead);
+            s.score += 1;
+            Some(s)
+        };
+
+        for next_state in [Some(turn_cw), Some(turn_ccw), forward] {
+            match next_state {
+                Some(next_state) if next_state.location == endpoint && next_state.score < best => {
+                    println!("Part 1: {}", next_state.score);
+                    break 'mainloop;
+                    //best = next_state.score;
+                    //best_paths.extend(next_state.visited);
+                }
+                Some(next_state) if next_state.location == endpoint && next_state.score == best => {
+                    //best_paths.extend(next_state.visited);
+                }
+                Some(next_state) if next_state.score > best => (),
+                Some(next_state) => {
+                    pq.push(next_state.clone(), Reverse(next_state.score));
+                }
+                None => (),
+            }
+        }
+        //Some(next_state) if visited.get((next_state.location, next_state.direction)).map(|v|v < next_state.score).unwrap_or(true) => {
+        //    visited.insert((next_state.location, next_state.direction), next_state.score);
+    }
+
+    //println!("Part 2: {}", best_paths.len());
+
+    let end = Instant::now();
+    println!("in {}μs", (end - start).as_micros());
+}
+
+fn main() {
+    let example = "\
+#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################";
+
+    let real = include_str!("../../in/16.txt").trim();
+
+    println!("--- example ---");
+    run(example);
+
+    println!("--- real ---");
+    run(real);
+}
