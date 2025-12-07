cf07285359ec54458bc905410a11b048f19cb842
Andrew Nowak <andrew@nowak.me.uk>
2025-12-07 16:49:22 +0000

    2025 day 7 part 2

--- 2025/src/bin/day7.rs
+++ 2025/src/bin/day7.rs
@@ -1,10 +1,10 @@
 use std::time::Instant;
 
-use aoclib::hset;
+use aoclib::{hmap, hset};
 use itertools::Itertools;
-use rustc_hash::FxHashSet;
+use rustc_hash::{FxHashMap, FxHashSet};
 
-fn iterate_splits(
+fn iterate_classic_manifold(
     (active_tachyons, times_split): (FxHashSet<usize>, usize),
     manifold_line: &&[u8],
 ) -> (FxHashSet<usize>, usize) {
@@ -22,6 +22,30 @@ fn iterate_splits(
     }
 
     (next_tachyons, times_split + splits)
+}
+
+fn upsert_timelines(next: &mut FxHashMap<usize, u64>, key: usize, timelines: &u64) {
+    next.entry(key)
+        .and_modify(|o| *o += timelines)
+        .or_insert(*timelines);
+}
+
+fn iterate_quantum_manifold(
+    active_tachyons: FxHashMap<usize, u64>,
+    manifold_line: &&[u8],
+) -> FxHashMap<usize, u64> {
+    let mut next_tachyons =
+        FxHashMap::with_capacity_and_hasher(active_tachyons.len() * 2, Default::default());
+
+    for (&tachyon_pos, &timelines) in active_tachyons.iter() {
+        if manifold_line[tachyon_pos] == b'^' {
+            upsert_timelines(&mut next_tachyons, tachyon_pos - 1, &timelines);
+            upsert_timelines(&mut next_tachyons, tachyon_pos + 1, &timelines);
+        } else {
+            upsert_timelines(&mut next_tachyons, tachyon_pos, &timelines);
+        }
+    }
+    next_tachyons
 }
 
 fn run(data: &str) {
@@ -42,13 +66,20 @@ fn run(data: &str) {
         .iter()
         .step_by(2)
         .dropping(1)
-        .fold((active_tachyons, 0), iterate_splits);
+        .fold((active_tachyons, 0), iterate_classic_manifold);
 
     println!("Part 1: {}", times_split);
 
-    // let p2 = format!("hello {} v2", data);
-    //
-    // println!("Part 2: {}", p2);
+    let active_tachyons = hmap!(start => 1);
+
+    let timelines_by_final_pos = lines
+        .iter()
+        .step_by(2)
+        .dropping(1)
+        .fold(active_tachyons, iterate_quantum_manifold);
+
+    let total_timelines: u64 = timelines_by_final_pos.values().sum();
+    println!("Part 2: {}", total_timelines);
 
     let end = Instant::now();
     println!("in {}μs", (end - start_time).as_micros());
